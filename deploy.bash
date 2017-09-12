4ad7235fef4510078b8d76acc1fd00f89b86e336
Brian <brian@quantrocket.com>
2017-09-12 19:50:36 -0400

    add bitbucket-pipelines.yml

---
+++ deploy.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# PYPI_USERNAME - (Requried) Username for the publisher's account on PyPI
+# PYPI_PASSWORD - (Required, Secret) Password for the publisher's account on PyPI
+
+cat <<EOF >> ~/.pypirc
+[distutils]
+index-servers=pypi
+
+[pypi]
+repository=https://pypi.python.org/pypi
+username=$PYPI_USERNAME
+password=$PYPI_PASSWORD
+EOF
+
+# Deploy to pip
+python setup.py sdist bdist_wheel
+twine upload dist/*
