bf72dfd3b2ecde950698995f0a569d2997ebb273
Brian <brian@quantrocket.com>
2025-10-23 17:12:53 -0400

    MAINT: update versioneer

--- moonchart/__init__.py
+++ moonchart/__init__.py
@@ -26,9 +26,8 @@ Modules
 utils
     Utility functions for performance analysis.
 """
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from .utils import set_default_palette
 set_default_palette()
